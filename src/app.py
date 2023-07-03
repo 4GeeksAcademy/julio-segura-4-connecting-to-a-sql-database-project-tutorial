846799a86df1c93a90c69e3b0fd85e26dc996fe0
Julio <jsmillon@protonmail.com>
2023-07-03 14:07:46 +0000

    commit

--- src/app.py
+++ src/app.py
@@ -3,13 +3,27 @@ from sqlalchemy import create_engine
 import pandas as pd
 from dotenv import load_dotenv
 
-# load the .env file variables
 load_dotenv()
 
 # 1) Connect to the database here using the SQLAlchemy's create_engine function
+connection_string = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}"
+engine = create_engine(connection_string).execution_options(autocommit=True)
+engine.connect()
 
 # 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
+engine.execute("""
+CREATE TABLE IF NOT EXISTS publishers(
+    publisher_id INT NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    PRIMARY KEY(publisher_id)
+);
+""")
+## Rest of sentences of create.sql script
 
 # 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+engine.execute("INSERT INTO publishers(publisher_id,name) values (1,'O Reilly Media');")
+## Rest of sentences of insert.sql script
 
 # 4) Use pandas to print one of the tables as dataframes using read_sql function
+result_dataFrame = pd.read_sql("Select * from publishers;", engine)
+print(result_dataFrame)
